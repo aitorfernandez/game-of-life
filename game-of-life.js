a65cea192bffeec219bad60cb1cb2b68f24151e7
Aitor Fernandez <aitor@qad.re>
2019-09-02 19:10:16 +0100

    playing with deaths and opacity

--- game-of-life.js
+++ game-of-life.js
@@ -1,5 +1,5 @@
 export default (p) => {
-  const resolution = 30
+  const resolution = 60
 
   const width = 1020 + resolution
   const height = 640 + resolution
@@ -8,21 +8,30 @@ export default (p) => {
   let cols
   let rows
 
-  function createCell(x, y, state) {
-    const color = state ? p.color(`rgba(0, 0, 0, 1)`) : p.color(`rgba(255, 255, 255, 1)`)
+  function createCell(x, y, state, deaths) {
     const pos = p.createVector(x * resolution, y * resolution)
 
     function draw() {
+      const opacity = 1 - (((deaths < 0 ? 0 : deaths > 100 ? 100 : deaths) / 100) * 2.5)
+      const color = state ? p.color(`rgba(0, 0, 0, ${opacity})`) : p.color(`rgba(245, 245, 245, ${opacity})`)
+
       p.noStroke()
 
       p.fill(color)
       p.smooth()
 
       const offset = resolution / 2
-      p.circle(pos.x + offset, pos.y + offset, offset)
+
+      let radius = opacity * 25
+      if (radius < 0) {
+        radius = radius * -1
+      }
+
+      p.circle(pos.x + offset, pos.y + offset, radius > 60 ? offset : radius)
     }
 
     return {
+      deaths,
       draw,
       state
     }
@@ -66,19 +75,20 @@ export default (p) => {
 
     for (let i = 0; i < cols; i += 1) {
       for (let j = 0; j < rows; j += 1) {
-        grid[i][j] = createCell(i, j, p.floor(p.random(2)))
+        const state = p.floor(p.random(2))
+        grid[i][j] = createCell(i, j, state, state ? 0 : 1)
       }
     }
   }
 
   function setup() {
     p.createCanvas(width, height)
-    p.frameRate(3)
+    p.frameRate(6)
     reset()
   }
 
   function draw() {
-    p.background('whiteSmoke')
+    p.background('white')
 
     for (let i = 0; i < cols; i += 1) {
       for (let j = 0; j < rows; j += 1) {
@@ -91,7 +101,10 @@ export default (p) => {
 
     for (let i = 0; i < cols; i += 1) {
       for (let j = 0; j < rows; j += 1) {
-        const state = grid[i][j].state
+        const {
+          deaths,
+          state
+        } = grid[i][j]
 
         const neighbours = countNeighbours(grid, i, j)
 
@@ -99,21 +112,21 @@ export default (p) => {
 
         // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
         if (state === 1 && neighbours < 2) {
-          next[i][j] = createCell(i, j, 0)
+          next[i][j] = createCell(i, j, 0, deaths + 1)
 
         // Any live cell with two or three live neighbours lives on to the next generation.
         } else if (state === 1 && (neighbours === 2 || neighbours === 3)) {
-          next[i][j] = createCell(i, j, 1)
+          next[i][j] = createCell(i, j, 1, deaths - 1)
 
         // Any live cell with more than three live neighbours dies, as if by overpopulation.
         } else if (state === 1 && neighbours > 3) {
-          next[i][j] = createCell(i, j, 0)
+          next[i][j] = createCell(i, j, 0, deaths + 1)
 
         // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
         }  else if (state === 0 && neighbours === 3) {
-          next[i][j] = createCell(i, j, 1)
+          next[i][j] = createCell(i, j, 1, deaths - 1)
         } else {
-          next[i][j] = createCell(i, j, state)
+          next[i][j] = createCell(i, j, state, state ? deaths - 1 : deaths + 1)
         }
       }
     }
